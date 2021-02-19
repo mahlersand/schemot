311e1010d846285d5e5e7779400d9ebcea5041b3
Samuel Mahler <mahlersand@gmail.com>
2021-02-19 11:33:19 +0100

    - Updated gitignore, removed excess file - Fixed an unlicensed file
    Signed-off-by: Samuel Mahler <mahlersand@gmail.com>

--- ParameterPacks.h
+++ ParameterPacks.h
@@ -1,3 +1,10 @@
+/*This file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html*/
+
 #pragma once
 
 #include "SchemotPack.h"
